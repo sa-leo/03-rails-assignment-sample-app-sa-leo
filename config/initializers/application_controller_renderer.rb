56b0ddeed1d55062ce39d77dfcde6d561a74c974
saki <yanosaki@yano-no-MacBook-Air.local>
2020-09-27 11:47:17 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6454f38ee0382b2519e1db87a941224027de47ba1eb4c362385e68eada7efc13f7e059a3ccdb166da025fd6e7cc1cde576507ecc2080a0bd23d9eac617a86729'
